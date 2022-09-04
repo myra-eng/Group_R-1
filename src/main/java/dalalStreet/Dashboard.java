a0144c6854d8345cbdfc56f854f7fe6ddc64a722
vivek pimprikar <pimprikar.vivek555@gmail.com>
2022-09-04 17:52:39 +0530

    ss
    ayess

--- src/main/java/dalalStreet/Dashboard.java
+++ src/main/java/dalalStreet/Dashboard.java
@@ -23,5 +23,15 @@ public class Dashboard {
 	private WebElement portfolio;
 	public void portfolio() {
 		portfolio.click();
+	}@FindBy (xpath="//a[@href='/exchanges']")
+	private WebElement exchange;
+	public void exchange() {
+		exchange.click();
 	}
+	@FindBy (xpath="//a[@href='/portfolio']")
+	private WebElement portfolio;
+	public void portfolio() {
+		portfolio.click();
+	}
+	
 }
